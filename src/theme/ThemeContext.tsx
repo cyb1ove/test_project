2e1415a3e5cb652392090171d71081787562bc86
cyb1ove <cyb1ove.ua@gmail.com>
2023-02-02 17:28:41 +0200

    initial commit

---
+++ src/theme/ThemeContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, FC, useState } from "react";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+interface ThemeContextProps {
+  theme?: Theme;
+  setTheme?: (theme: Theme) => void;
+}
+
+export const ThemeContext = createContext<ThemeContextProps>({});
+export const LOCAL_STORAGE_THEME_KEY = "theme";
+
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+const ThemeProvider: FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
