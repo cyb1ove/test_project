2778206bfd98ce81cd3e44104efbf4e7d8c699c3
cyb1ove <cyb1ove.ua@gmail.com>
2023-02-25 16:00:21 +0200

    initial redux provider created

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import './shared/config/i18n/i18n';
 
 import { ErrorBoundary } from 'app/providers/ErrorBoundary/ui/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,12 +9,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 
 render(
-  <ErrorBoundary>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </ErrorBoundary>,
+  <StoreProvider>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </StoreProvider>,
   document.querySelector('#root')
 );
